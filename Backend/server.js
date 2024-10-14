953d0344851e4cd3a0469b70f7fdf42787952d41
Praveen pramodh <149309291+praveenpramodh2002@users.noreply.github.com>
2024-10-14 18:19:58 +0530

    ghgbjhb

--- Backend/server.js
+++ Backend/server.js
@@ -1,18 +1,12 @@
-//server.js
 const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const router1 = require('./Taskmanagement/router');  // Task management routes
-const router2 = require('./AccountManagement/router');  // Account management routes
-const router3 = require('./Inventorymanagement/router');  // Inventory management routes
-const supplierRoutes = require("./SuppilerManagement/SupplierRoutes");
-const deletedSupplierRoutes = require("./SuppilerManagement/DeletedSupplierRoutes");
-const productRequestRoutes = require("./SuppilerManagement/ProductRequestRoutes")
-
-
 const app = express();
+const cors = require('cors');
 const port = 3001;
 const host = 'localhost';
+const mongoose = require('mongoose');
+const router1 = require('./Taskmanagement/router'); 
+const router2 = require('./AccountManagement/router'); 
+const router3 = require('./Inventorymanagement/router');
 const twilio = require('twilio');
 
 // Import routes from Appointment
@@ -20,8 +14,8 @@ const { customerRouter, serviceRouter, bookingRouter } = require('./Appoitment/s
 
 // Middleware for CORS
 app.use(cors({
-    origin: 'http://localhost:3000', // Allow requests from this origin
-    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
+    origin: 'http://localhost:3000', 
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], 
     credentials: true,
 }));
 
@@ -31,7 +25,6 @@ app.use(express.json());
 
 // MongoDB Connection
 const uri = 'mongodb+srv://praveenpramodh2002:Praveengamage@cluster0.oxfpe.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
-
 const connect = async () => {
     try {
         await mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -40,21 +33,25 @@ const connect = async () => {
         console.error('MongoDB Error:', error);
     }
 };
-
 connect();
 
 // Use Routers
 app.use('/api', router1); // Routes for task management
 app.use('/api', router2);
-app.use('/api',router3) // Routes for account management
+app.use('/api', router3); // Routes for account management
+
+// Add appointment routes
+app.use('/customer', customerRouter);
+app.use('/service', serviceRouter);
+app.use('/booking', bookingRouter);
 
 // Error Handling Middleware
 app.use((err, req, res, next) => {
     console.error(err.stack);
-    res.status(500).send('Something broke!'); // Send a 500 status for server errors
+    res.status(500).send('Something broke!');
 });
 
-// Start the server
+// Start Server
 app.listen(port, host, () => {
     console.log(`Node server is listening on http://${host}:${port}`);
 });
